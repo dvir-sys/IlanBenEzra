eed7760988aa29aba2b8e4d1f6fafe3e65c66674
iMelki <melki@post.bgu.ac.il>
2021-06-27 15:42:41 +0300

    router etc

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: '', component: DashboardComponent},
+  {path: 'home', component: DashboardComponent},/*
+  {path: 'lists', component: ListsComponent},
+  {path: 'lists/:id', component: ListDetailComponent},
+  {path: 'lists/:id/edit', component: ListDetailComponent},
+  {path: 'items', component: ListsComponent},
+  {path: 'messages', component: MessagesComponent},*/
+  {path: '**', component: DashboardComponent, pathMatch: 'full'} //TODO: Change to Error message page! 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
